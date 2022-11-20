46b46b2efd565e0a2f3b7b50d8ef353dd0d62903
ZebLawrence <zeblawrence@gmail.com>
2022-11-20 10:28:07 -0700

    More options better perf, remove glyph component

--- src/RainDrop.js
+++ src/RainDrop.js
@@ -1,22 +1,42 @@
-import { memo } from 'react';
-import Glyph from './Glyph';
-import './rainDrop.scss';
+import { memo, useRef, useLayoutEffect } from 'react';
+import glyphData from './glyphData.json';
 
-function RainDrop({ emHeight, speed, changeRate }) {
-  const glyphs = [];
+function RainDrop({ emHeight, speed, changeRate, glyphShadow }) {
+  const dropRef = useRef();
+  const drop = [];
+  const rand = Math.floor(Math.random() * 40);
+  const { glyphs } = glyphData;
+  const rate = (changeRate * 100);
+  const rateHalf = Math.floor(rate / 2);
+  const timeout = Math.floor((Math.random() * (rate - rateHalf + 1)) + rateHalf);
 
   for (let index = 0; index < emHeight; index++) {
-    glyphs.push(<Glyph changeRate={changeRate} key={`glyph-${index}`} />);
+    drop.push(
+      `${glyphs[Math.floor(Math.random() * glyphs.length)]}`
+    );
   }
 
-  const rand = Math.floor(Math.random() * 40);
-  const rainClass = `rain-drop raindrop-${rand}`;
+  useLayoutEffect(() => {
+    const timer = setInterval(() => {
+      if (dropRef && dropRef.current) {
+        const deleteIndex = Math.floor(Math.random() * drop.length);
+        const newGlyphIndex = Math.floor(Math.random() * glyphs.length);
+        drop.splice(deleteIndex, 1, `${glyphs[newGlyphIndex]}`);
+        dropRef.current.innerHTML = drop.join('\n');      
+      }
+    }, timeout);
 
-  const animation = `raining infinite ${rand + speed}s`
-// className={rainClass}
+    return () => {
+      clearInterval(timer);
+    };
+  }, [emHeight, changeRate]);
+
   return (
-    <div className={rainClass} style={{ animationDuration: `${rand + speed}s` }}>
-      {glyphs}
+    <div
+      ref={dropRef}
+      className={`rain-drop ${glyphShadow ? 'shadow' : ''}`}
+      style={{ animationDuration: `${rand + speed}s` }}>
+      {drop.join('\n')}
     </div>
   );
 }
